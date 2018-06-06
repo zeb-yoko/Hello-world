e5759c54021e44426895edd4205a172cd6bdbb35
Zeb <zebadiah.yoko@gmail.com>
2018-06-06 14:07:45 -0500

    Add files via upload

---
+++ startup-asus.R
@@ -0,0 +1,17 @@
+library(car)
+library(dhglm)
+library(dplyr)
+library(effects)
+library(ggplot2)
+library(glmm)
+library(gridExtra)
+library(lme4)
+library(lmerTest)
+library(mcmc)
+library(plotly)
+library(reshape2)
+#setwd("C:/Users/zebadiah/NDSU Dropbox/Dropbox/Geum triflorum/Master_Datasheets")
+##ASUS
+setwd("C:/Users/zippy/Dropbox/Geum triflorum/Master_Datasheets")
+#set colors   #Prairie   #GL ALvar  #MB Alvar
+col.esa <- c("#18563E", "#82BE42", "#FFC423")
